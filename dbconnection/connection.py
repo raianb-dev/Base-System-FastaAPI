c3b301bf259c89e1ae336bd176a065429c082ce5
Raian Barbosa <raianpbstudio@gmail.com>
2022-11-10 12:25:23 -0300

    .add finish --version 0.1

--- dbconnection/connection.py
+++ dbconnection/connection.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://jhbiockfkomjxn:6ddbedefceb3f216b159a483d8f88429ecaeae03a4078785bec471209ee12f66@ec2-52-70-45-163.compute-1.amazonaws.com:5432/daund6m8qjmt1p"
+SQLALCHEMY_DATABASE_URL = ""
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 metadata = sqlalchemy.MetaData()
